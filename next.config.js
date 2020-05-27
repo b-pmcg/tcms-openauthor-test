ec2f8b40841ea1c7fc70037d82316e52e2b5d63f
Phil Bain <bain.pmcg@gmail.com>
2020-05-27 17:07:17 +0200

    wip markdown

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+const glob = require("glob");
 
 module.exports = {
   env: {
@@ -6,4 +6,56 @@ module.exports = {
     REPO_FULL_NAME: process.env.REPO_FULL_NAME,
     BASE_BRANCH: process.env.BASE_BRANCH,
   },
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
+  // exportPathMap: async function() {
+  //   const routes = {
+  //     '/': { page: '/' },
+  //     '/info': { page: '/info' },
+  //   }
+  //   //get all .md files in the posts dir
+  //   const blogs = glob.sync('posts/**/*.md')
+
+  //   //remove path and extension to leave filename only
+  //   const blogSlugs = blogs.map(file =>
+  //     file
+  //       .split('/')[1]
+  //       .replace(/ /g, '-')
+  //       .slice(0, -3)
+  //       .trim()
+  //   )
+
+  //   //add each blog to the routes obj
+  //   blogSlugs.forEach(blog => {
+  //     routes[`/blog/${blog}`] = { page: '/blog/[slug]', query: { slug: blog } }
+  //   })
+
+  //   return routes
+  // },
 };
+
+//OLD MDX VERSION
+
+// require("dotenv").config();
+
+// const withMDX = require("@next/mdx")({
+//   extension: /\.(mdx)$/,
+//   // options: {
+//   //   remarkPlugins: [slug],
+//   //   compilers: [mdxTableOfContents],
+//   // },
+// });
+
+// module.exports = withMDX({
+//   // pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+//   env: {
+//     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+//     REPO_FULL_NAME: process.env.REPO_FULL_NAME,
+//     BASE_BRANCH: process.env.BASE_BRANCH,
+//   },
+// });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,30 @@
 import Head from "next/head";
+import fs from "fs";
+import { join } from "path";
+import matter from "gray-matter";
 
-/**
- * Import helpers and GetStaticProps type
- */
-import { getGithubPreviewProps, parseJson } from "next-tinacms-github";
+// tinaCMS helper
+import {
+  getGithubPreviewProps,
+  parseJson,
+  parseMarkdown,
+} from "next-tinacms-github";
 import { GetStaticProps } from "next";
-import { useGithubJsonForm } from "react-tinacms-github";
+import { useGithubJsonForm, useGithubMarkdownForm } from "react-tinacms-github";
 
 export default function Home({ file }) {
   const formOptions = {
     label: "Home Page",
     fields: [{ name: "title", component: "text" }],
   };
-
-  // Registers a JSON Tina Form
-  const [data, form] = useGithubJsonForm(file, formOptions);
+  // const formOptions = {
+  //   label: "Home Page",
+  //   fields: [{ name: "title", component: "text" }],
+  // };
+  const [data, form] = useGithubMarkdownForm(file, formOptions);
+  // const [data, form] = useGithubJsonForm(file, formOptions);
+  // console.log("from", form);
+  // console.log("data", data);
   return (
     <div className="container">
       <Head>
@@ -28,6 +38,7 @@ export default function Home({ file }) {
            * Render the title from `home.json`
            */}
           {data.title}
+          {data.component}
         </h1>
 
         <p className="description">
@@ -75,152 +86,6 @@ export default function Home({ file }) {
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
@@ -228,15 +93,24 @@ export default function Home({ file }) {
 /**
  * Fetch data with getStaticProps based on 'preview' mode
  */
-export const getStaticProps: GetStaticProps = async function ({
-  preview,
-  previewData,
-}) {
+export const getStaticProps: GetStaticProps = async function (passedProps) {
+  const { preview, previewData } = passedProps;
+  // console.log("^^^passedProps", passedProps);
+
+  // const fileContents = fs.readFileSync(fullPath, "utf8");
+  // const fileContents = (await import("../content/sample.md")).default;
+  // console.log("fileContents", fileContents);
+  // const { data, content } = matter(fileContents);
+  // console.log("data", data, content);
+  // console.log("data", data);
+
   if (preview) {
     return getGithubPreviewProps({
       ...previewData,
-      fileRelativePath: "content/home.json",
-      parse: parseJson,
+      fileRelativePath: "content/sample.md",
+      parse: parseMarkdown,
+      // fileRelativePath: "content/home.json",
+      // parse: parseJson,
     });
   }
   return {
@@ -245,8 +119,10 @@ export const getStaticProps: GetStaticProps = async function ({
       error: null,
       preview: false,
       file: {
-        fileRelativePath: "content/home.json",
-        data: (await import("../content/home.json")).default,
+        fileRelativePath: "content/sample.md",
+        data: (await import("../content/sample.md")).default,
+        // fileRelativePath: "content/home.json",
+        // data: (await import("../content/home.json")).default,
       },
     },
   };
